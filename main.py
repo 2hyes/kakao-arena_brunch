fd54caad42815e83fcbd91bd913f08901727fdb0
2hyes <36873744+2hyes@users.noreply.github.com>
2020-08-06 03:54:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import preprocessing
+import recommender
+
 ## dev
-target_users_list, users, metadata, magazine, read, read_each_article = data_preprocessing('dev')
-recommed = recommender(target_users_list, './recommend.txt')
+target_users_list, users, metadata, magazine, read, read_each_article = preprocessing.data_preprocessing('dev')
+recommed = recommender.recommender(target_users_list, './recommend.txt')
 
 ## test
 #target_users_list, users, metadata, magazine, read, read_each_article = data_preprocessing('test')
