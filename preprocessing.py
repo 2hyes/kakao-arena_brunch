5e1069e242bba03779afe1904d87776aacbb1857
2hyes <36873744+2hyes@users.noreply.github.com>
2020-08-06 03:52:34 +0900

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,8 @@
+import data_loading
+
+"""
+데이터 프레임 컬럼명 통일
+"""
 def column_name_fixing(users, magazine, metadata):
     users.rename(columns={"id":"user_id"}, inplace = True)
     magazine.rename(columns={"id":"magazine_id"}, inplace = True)
@@ -5,8 +10,7 @@ def column_name_fixing(users, magazine, metadata):
     return users, magazine, metadata
     
 #users, magazine, metadata = column_name_fixing(users, magazine, metadata)
-
-def counting(users, read):
+def counting(users):
     # ascending = False: 점수가 높을 수록 상위 순위
     # method='dense': 동점자에게 같은 순위 부여 / 그룹 간 순위가 1씩 증가
     """
@@ -20,12 +24,21 @@ def counting(users, read):
     ['article_cnt']: 해당 사용자가 읽은 글의 수 col 생성
     랭킹 col 생성 x (시간 당 읽은 글의 수--> 사용자id 중복)
 
-    """
+    
     read['article_cnt'] = read['article_id'].map(len)
+    """
+    print('users preprocessing completed!', '\n')
+    return users
 
-    return users, read
+#users = counting(users)
 
-#users, read = counting(users, read)
+
+## 숫자형식의 날짜를 입력하면 unix 시간으로 변환
+def get_unix_time(reg_ts):
+    string_time = str(reg_ts)[:4]+'-'+str(reg_ts)[4:6]+'-'+str(reg_ts)[6:8]+' 00:00:00'
+    unix_time = time.mktime(datetime.datetime.strptime(string_time, '%Y-%m-%d %H:%M:%S').timetuple())*1000
+    
+    return unix_time
 
 """
 metadata preprocessing
@@ -36,6 +49,8 @@ def unix_to_timestamp(metadata):
     metadata['date'] = metadata.reg_ts.apply(lambda d: datetime.datetime.fromtimestamp(int(d)).strftime('%Y-%m-%d'))
     metadata['hour'] = metadata.reg_ts.apply(lambda d: datetime.datetime.fromtimestamp(int(d)).strftime('%H:%M:%S'))
     metadata['weekday'] = metadata.reg_ts.apply(lambda d: datetime.datetime.fromtimestamp(int(d)).strftime('%a'))
+    metadata.reg_ts *= 1000.0  
+    print('metadata.reg_ts preprocessing completed!', '\n')
     return metadata
 
 #metadata = unix_to_timestamp(metadata)
@@ -44,16 +59,16 @@ def unix_to_timestamp(metadata):
 metadata preprocessing
 : 일정 기간 동안의 조회수 recent_view / 전체 기간 동안의 조회수 view 생성
 """
-def metadata_preprocessing(metadata, read_each_article, from_dt, to_dt):
+def metadata_preprocessing(metadata, read, from_dt, to_dt):
     # 전체 조회수 view
-    view = read_each_article.groupby('article_id').count()['user_id']
+    view = read.groupby('article_id').count()['user_id']
     view_df = pd.DataFrame({'id':view.index, 'view':view.values})
     metadata = pd.merge(metadata, view_df, how='left', on='id')
     metadata['view'] = metadata['view'].fillna(0)
     
     # 최근 조회수 recent_view
-    dt = pd.to_numeric(read_each_article['date'])
-    partial_read = read_each_article[(dt >= from_dt) & (dt <= to_dt)]
+    dt = pd.to_numeric(read['date'])
+    partial_read = read[(dt >= from_dt) & (dt <= to_dt)]
     recent_view = partial_read.groupby('article_id').count()['user_id']
     recent_view_df = pd.DataFrame({'id':recent_view.index, 'recent_view':recent_view.values})
     metadata = pd.merge(metadata, recent_view_df, how='left', on='id')
@@ -61,10 +76,6 @@ def metadata_preprocessing(metadata, read_each_article, from_dt, to_dt):
     print('metadata preprocessing completed!', '\n')
 
     return metadata
-
-
-# 최근(2주로 설정) 조회수 와 전체 조회수 생성
-#metadata = metadata_preprocessing(metadata, read_each_article, 20190215, 20190228)
 
 """
 read preprocessing
@@ -84,17 +95,18 @@ def read_preprocessing(read):
 """
 전체 전처리 함수 실행
 """
-
 def data_preprocessing(mode):
     if mode == 'dev':
-       target_users_list, _, users, metadata, magazine, read, read_each_article = training_loading()
+       target_users_list, _, users, metadata, magazine, read = data_loading.training_loading()
     elif mode == 'test':
-        _, target_users_list, users, metadata, magazine, read, read_each_article = training_loading()
+        _, target_users_list, users, metadata, magazine, read = data_loading.training_loading()
     
     users, magazine, metadata = column_name_fixing(users, magazine, metadata)
-    users, read = counting(users, read)
+    users = counting(users)
     metadata = unix_to_timestamp(metadata)
-    metadata = metadata_preprocessing(metadata, read_each_article, 20190215, 20190228)
+    metadata = metadata_preprocessing(metadata, read, 20190215, 20190228)
     read = read_preprocessing(read)
 
-    return target_users_list, users, metadata, magazine, read, read_each_article
+    
+    print('Preprocessing COMPLETED!\n')
+    return target_users_list, users, metadata, magazine, read
